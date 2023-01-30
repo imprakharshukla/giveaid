e097964d040844e151d6704cb4b07d1110a9f642
Prakhar Shukla <imprakharshukla@gmail.com>
2023-01-30 19:18:25 +0530

    second commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,24 @@
-import {IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar} from '@ionic/react';
+import {IonContent, IonPage} from '@ionic/react';
 import './Tab1.css';
 import React from "react";
 
 const Tab1: React.FC = () => {
     return (
         <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Tab 1</IonTitle>
-                </IonToolbar>
-            </IonHeader>
             <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Dashboard</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <div className={"flex justify-center items-center bg-green-400"}>
-                    <IonText>
-                        <h1>Giveaid</h1>
-                    </IonText>
+                <div
+                    className={"flex font-sans flex-col justify-center items-center bg-primary space-y-6 px-4 py-5 rounded-b-3xl"}>
+                    <h1 className={"text-white font-extrabold"}>Giveaids</h1>
+                    <div className="mb-6">
+                        <input type="email" id="email"
+                               className="bg-secondary text-white font-semibold ring-0 focus:ring-0 focus:ring-offset-0 border-0 placeholder-white text-sm rounded-lg block w-full px-4 py-5"
+                               placeholder="Select Pickup Location" required/>
+                    </div>
+                    <div className="mb-6">
+                        <input type="email" id="email"
+                               className="bg-secondary text-white font-semibold ring-0 focus:ring-0 focus:ring-offset-0 border-0 placeholder-white text-sm rounded-lg block w-full px-4 py-5"
+                               placeholder="Select Pickup Location" required/>
+                    </div>
                 </div>
             </IonContent>
         </IonPage>
