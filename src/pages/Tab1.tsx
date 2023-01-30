6a07a0a3248e0ccc57e32a263c4aca3998567439
Prakhar Shukla <imprakharshukla@gmail.com>
2023-01-30 18:36:26 +0530

    initial commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar} from '@ionic/react';
 import './Tab1.css';
+import React from "react";
 
 const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tab 1</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Dashboard</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <div className={"flex justify-center items-center bg-green-400"}>
+                    <IonText>
+                        <h1>Giveaid</h1>
+                    </IonText>
+                </div>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab1;
